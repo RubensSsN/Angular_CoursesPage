523f5754bc5bc57e2284b2ba8efcb2927630027b
RubensSsN <contatorubeno@gmail.com>
2022-11-20 01:21:56 -0300

    Service implement

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 
@@ -8,15 +9,13 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Spring', category: 'Back-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category']
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
-
+    this.courses = this.coursesService.list();
   }
 
 }
