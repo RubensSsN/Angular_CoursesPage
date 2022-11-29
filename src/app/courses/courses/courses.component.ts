4a5b4b8d5d215503d9e1ba8f73371a1894f5d276
RubensSsN <contatorubeno@gmail.com>
2022-11-29 20:18:48 -0300

    Tratamento de erros

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from '../model/course';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { CoursesService } from './../services/courses.service';
 
 @Component({
@@ -13,13 +15,23 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
+        this.onError('Erro ao carregar cursos');
         return of([])
       })
     );
+  }
+
+  onError(errorMsg : string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {}
