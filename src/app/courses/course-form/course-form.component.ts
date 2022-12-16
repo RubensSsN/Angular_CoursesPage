97c5f4c348e52b12f63129e3a14c5ba963aac183
RubensSsN <contatorubeno@gmail.com>
2022-12-16 13:03:43 -0300

    Method submit onClick

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Action } from 'rxjs/internal/scheduler/Action';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +14,9 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar) {
     this.form = formBuilder.group({
       name: [null],
       category: [null]
@@ -21,11 +27,15 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
-
+    this.service.save(this.form.value)
+    .subscribe(result => console.log(result), error => this.onError()); // Está fazendo com que ao se usar este metodo seja salvo os dados do formulário dando subscribe com seu resultado e também se tiver algum erro exibindo o erro com uma snack bar
   }
 
   onCancel() {
 
   }
 
+  onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 5000 }); //Ao se ter um erro ele é acionado se o método usar ele e mostra uma snackbar na tela do usuário.
+  }
 }
