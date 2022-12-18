9c5eee7cc0723659f84ec48f2cf9a2cc41e182e8
RubensSsN <contatorubeno@gmail.com>
2022-12-17 22:46:32 -0300

    Method post (Create) update with ResponseEntity

--- CRUD-Spring/src/main/java/com/rubens/crudspring/controller/CourseController.java
+++ CRUD-Spring/src/main/java/com/rubens/crudspring/controller/CourseController.java
@@ -3,6 +3,8 @@ package com.rubens.crudspring.controller;
 import com.rubens.crudspring.model.Course;
 import com.rubens.crudspring.repository.CoursesRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,9 +26,16 @@ public class CourseController {
   }
 
   @PostMapping
-  @Transactional
+  public ResponseEntity<Course> salvar(@RequestBody Course curso) { //Método para salvar dados no banco de dados do tipo Course e que retornará o HTTP 201 (Created) por conta do ResponseEntity.
+    return ResponseEntity.status(HttpStatus.CREATED)
+      .body(coursesRepository.save(curso));
+  }
+
+  //Uma maneira de fazermos resumidamente o ResponseEntity, porém a diferença é que não poderemos alterar nem manusear os dados da reposta.
+  /*@PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
   public Course salvar(@RequestBody Course curso) {
     return coursesRepository.save(curso);
-  }
+  }*/
 
 }
