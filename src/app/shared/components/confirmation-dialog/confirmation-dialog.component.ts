f82db2f3307d7450252221e1960c50304177eb71
RubensSsN <contatorubeno@gmail.com>
2022-12-31 22:44:18 -0300

    Create component of the confirmation dialog.

---
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss'],
+})
+export class ConfirmationDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {}
+
+  ngOnInit(): void {}
+
+  onConfirm(result: boolean): void {
+    this.dialogRef.close(result); // Ao ser acionado com ele vem junto o valor da confirmação do usuário e ele fecha o dialog passando o resultado.
+  }
+}
