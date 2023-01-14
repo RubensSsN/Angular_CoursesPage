e45f77ffdbc32bc54cd32eafcce1d4ec1d7b50ae
RubensSsN <contatorubeno@gmail.com>
2023-01-14 11:38:37 -0300

    Soft Delete implementation.

--- CRUD-Spring/src/main/java/com/rubens/crudspring/model/Course.java
+++ CRUD-Spring/src/main/java/com/rubens/crudspring/model/Course.java
@@ -7,11 +7,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?") // Aqui podemos passar o comando SQL que o hibernate irá executar toda vez que chamarmos o método DELETE do nosso repository.
+@Where(clause = "status = 'Ativo'") //  toda vez que formos fazer um SELECT no nosso banco de dados o Hibernate automaticamente vai adicionar esse filtro na clausula WHERE.
 public class Course {
 
   @Id
@@ -31,5 +35,10 @@ public class Course {
   @Column(length = 12, nullable = false)
   private String category;
 
+  @NotNull
+  @Column(length = 10, nullable = false)
+  @Pattern(regexp = "Ativo|Inativo")
+  @Length(max = 10)
+  private String status = "Ativo";
 
 }
