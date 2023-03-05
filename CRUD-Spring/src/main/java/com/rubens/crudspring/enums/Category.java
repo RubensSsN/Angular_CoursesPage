0660df5cfa5f227288e78b9e1ef872245f0678e3
RubensSsN <contatorubeno@gmail.com>
2023-03-05 18:12:51 -0300

    Add Constructor for the enum Category

--- CRUD-Spring/src/main/java/com/rubens/crudspring/enums/Category.java
+++ CRUD-Spring/src/main/java/com/rubens/crudspring/enums/Category.java
@@ -2,6 +2,21 @@ package com.rubens.crudspring.enums;
 
 public enum Category {
 
-  BACKEND, FRONTEND
+  BACKEND("Back-end"), FRONTEND("Front-end");
+
+  private String value;
+
+  private Category(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
 
 }
